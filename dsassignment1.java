c1069df41844598016b516eb0c6f0e523559bc54
jake <130408715+Chichianamege@users.noreply.github.com>
2024-04-26 13:05:16 -0400

    Add files via upload

---
+++ dsassignment1.java
@@ -0,0 +1,11 @@
+// Name: Chidera Anamege
+// Class: CS 3305/ Section W01
+// Term: Spring 2024
+// Instructor: Carla McManus
+// Assignment: 1 Hello World
+
+public class dsassignment1 {
+        public static void main(String[] args) {
+            System.out.println("Hello World!");
+        }
+    }
