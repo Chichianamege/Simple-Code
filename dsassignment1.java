66047abd300332be5b6f5a3717920a18e391c24c
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:31:58 -0400

    Update dsassignment1.java

--- dsassignment1.java
+++ dsassignment1.java
@@ -1,8 +1,4 @@
 // Name: Chidera Anamege
-// Class: CS 3305/ Section W01
-// Term: Spring 2024
-// Instructor: Carla McManus
-// Assignment: 1 Hello World
 
 public class dsassignment1 {
         public static void main(String[] args) {
